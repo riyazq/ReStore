779439c335fcede20a05dce88baa70068ec4b477
Qureshi <riyazq@users.noreply.github.com>
2024-01-18 02:41:01 +0530

    End of Section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,7 +7,8 @@ import {
   createTheme,
 } from "@mui/material"
 import { Outlet } from "react-router-dom"
-
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 function App() {
   const [darkMode, setDarkMode] = useState(false)
   const palleteType = darkMode ? "dark" : "light"
@@ -24,6 +25,7 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -11,7 +11,9 @@ import {
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Product } from "../../app/models/product"
-import axios from "axios"
+import agent from "../../app/api/agent"
+import NotFound from "../../app/errors/NotFound"
+import LoadingComponent from "../../app/layout/LoadingComponent"
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>()
@@ -19,17 +21,17 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/Products/${id}`)
-      .then((response) => {
-        console.log(response.data)
-        setProduct(response.data)
-      })
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false))
+    id &&
+      agent.Catalog.details(parseInt(id!))
+        .then((response) => {
+          console.log(response)
+          setProduct(response)
+        })
+        .catch((error) => console.log(error.response))
+        .finally(() => setLoading(false))
   }, [id])
-  if (loading) return <h3>Loading...</h3>
-  if (!product) return <h3>Product Not Found</h3>
+  if (loading) return <LoadingComponent message="Loading Product" />
+  if (!product) return <NotFound />
 
   return (
     <Grid container spacing={6}>
